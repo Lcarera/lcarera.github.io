f169eb68f0c1be546a62977d26d5d9487d339010
lcarera <lcarera04@gmail.com>
2023-06-10 20:50:47 -0300

    adding reset btn

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var valorHoraInput = document.getElementById('valor-hora');
 var horasVirtualesInput = document.getElementById('horas-virtuales');
 var horasPresencialesInput = document.getElementById('horas-presenciales');
 var resultadoDiv = document.getElementById('resultado');
-
+var resetBtn = document.getElementById('refresh-button');
 // Load data from localStorage
 var data = JSON.parse(localStorage.getItem('data')) || {};
 
@@ -18,10 +18,11 @@ if (data.horasPresenciales) {
   horasPresencialesInput.value = data.horasPresenciales;
 }
 calcularSueldo();
+resetBtn.style.display = 'none';
 valorHoraInput.addEventListener('keyup', calcularSueldo);
 horasVirtualesInput.addEventListener('keyup', calcularSueldo);
 horasPresencialesInput.addEventListener('keyup', calcularSueldo);
-
+resetBtn.addEventListener('click', resetCalculadora);
 function calcularSueldo() {
   var valorHora = parseFloat(valorHoraInput.value);
   var horasVirtuales = parseFloat(horasVirtualesInput.value);
@@ -40,12 +41,26 @@ function calcularSueldo() {
     sueldo.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
   // Save data to localStorage
-  localStorage.setItem(
-    'data',
-    JSON.stringify({
-      valorHora: valorHora,
-      horasVirtuales: horasVirtuales,
-      horasPresenciales: horasPresenciales,
-    })
-  );
+  saveData(valorHora, horasVirtuales, horasPresenciales);
+  resetBtn.style.display = '';
+}
+
+function saveData(valorHora, horasVirtuales, horasPresenciales) {
+    localStorage.setItem(
+        'data',
+        JSON.stringify({
+            valorHora: valorHora,
+            horasVirtuales: horasVirtuales,
+            horasPresenciales: horasPresenciales,
+        })
+    );
+}
+
+function resetCalculadora() {
+    saveData(0,0,0)
+    valorHoraInput.value = null;
+    horasVirtualesInput.value = null;
+    horasPresencialesInput.value = null;
+    resultadoDiv.textContent = "";
+    resetBtn.style.display = 'none';
 }
