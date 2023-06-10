91356595e905760e2e433da34444fc6500390bcf
lcarera <lcarera04@gmail.com>
2023-06-10 20:18:07 -0300

    Saves data on browser and loads it on init

--- script.js
+++ script.js
@@ -3,21 +3,49 @@ var horasVirtualesInput = document.getElementById('horas-virtuales');
 var horasPresencialesInput = document.getElementById('horas-presenciales');
 var resultadoDiv = document.getElementById('resultado');
 
+// Load data from localStorage
+var data = JSON.parse(localStorage.getItem('data')) || {};
+
+if (data.valorHora) {
+  valorHoraInput.value = data.valorHora;
+}
+
+if (data.horasVirtuales) {
+  horasVirtualesInput.value = data.horasVirtuales;
+}
+
+if (data.horasPresenciales) {
+  horasPresencialesInput.value = data.horasPresenciales;
+}
+calcularSueldo();
 valorHoraInput.addEventListener('keyup', calcularSueldo);
 horasVirtualesInput.addEventListener('keyup', calcularSueldo);
 horasPresencialesInput.addEventListener('keyup', calcularSueldo);
 
 function calcularSueldo() {
-    var valorHora = parseFloat(valorHoraInput.value);
-    var horasVirtuales = parseFloat(horasVirtualesInput.value);
-    var horasPresenciales = parseFloat(horasPresencialesInput.value);
+  var valorHora = parseFloat(valorHoraInput.value);
+  var horasVirtuales = parseFloat(horasVirtualesInput.value);
+  var horasPresenciales = parseFloat(horasPresencialesInput.value);
 
-    if (isNaN(valorHora) || isNaN(horasVirtuales) || isNaN(horasPresenciales)) {
-        resultadoDiv.textContent = "";
-        return;
-    }
+  if (isNaN(valorHora) || isNaN(horasVirtuales) || isNaN(horasPresenciales)) {
+    resultadoDiv.textContent = "";
+    return;
+  }
 
-    var sueldo = (valorHora * horasVirtuales) + ((valorHora + 400) * horasPresenciales);
+  var sueldo =
+    valorHora * horasVirtuales + (valorHora + 400) * horasPresenciales;
 
-    resultadoDiv.textContent = "Sueldo: $" + sueldo.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  resultadoDiv.textContent =
+    "Sueldo: $" +
+    sueldo.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+  // Save data to localStorage
+  localStorage.setItem(
+    'data',
+    JSON.stringify({
+      valorHora: valorHora,
+      horasVirtuales: horasVirtuales,
+      horasPresenciales: horasPresenciales,
+    })
+  );
 }
