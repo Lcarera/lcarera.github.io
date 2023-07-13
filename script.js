9deccb10f1743604525fe37545b511899c21229f
lcarera <lcarera04@gmail.com>
2023-07-12 22:28:02 -0300

    nueva calc

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-var valorHoraInput = document.getElementById('valor-hora');
+var valorHoraVirtualInput = document.getElementById('valor-hora-virtual');
+var valorHoraPresencialInput = document.getElementById('valor-hora-presencial');
 var horasVirtualesInput = document.getElementById('horas-virtuales');
 var horasPresencialesInput = document.getElementById('horas-presenciales');
 var resultadoDiv = document.getElementById('resultado');
+var resultadoPresencialDiv = document.getElementById('resultado-presencial');
+var resultadoVirualDiv = document.getElementById('resultado-virtual');
 var resetBtn = document.getElementById('refresh-button');
 // Load data from localStorage
 var data = JSON.parse(localStorage.getItem('data')) || {};
 
-if (data.valorHora) {
-  valorHoraInput.value = data.valorHora;
+if (data.valorHoraVirtual) {
+  valorHoraVirtualInput.value = data.valorHoraVirtual;
+}
+
+if (data.valorHoraPrensecial) {
+  valorHoraPresencialInput.value = data.valorHoraPresencial;
 }
 
 if (data.horasVirtuales) {
@@ -19,37 +26,45 @@ if (data.horasPresenciales) {
 }
 calcularSueldo();
 resetBtn.style.display = 'none';
-valorHoraInput.addEventListener('keyup', calcularSueldo);
+valorHoraVirtualInput.addEventListener('keyup', calcularSueldo);
+valorHoraPresencialInput.addEventListener('keyup', calcularSueldo);
 horasVirtualesInput.addEventListener('keyup', calcularSueldo);
 horasPresencialesInput.addEventListener('keyup', calcularSueldo);
 resetBtn.addEventListener('click', resetCalculadora);
 function calcularSueldo() {
-  var valorHora = parseFloat(valorHoraInput.value);
+  var valorHoraPresencial = parseFloat(valorHoraPresencialInput.value);
+  var valorHoraVirtual = parseFloat(valorHoraVirtualInput.value);
   var horasVirtuales = parseFloat(horasVirtualesInput.value);
   var horasPresenciales = parseFloat(horasPresencialesInput.value);
 
-  if (isNaN(valorHora) || isNaN(horasVirtuales) || isNaN(horasPresenciales)) {
+  if (isNaN(valorHoraPresencial) || isNaN(valorHoraVirtual) || isNaN(horasVirtuales) || isNaN(horasPresenciales)) {
     resultadoDiv.textContent = "";
     return;
   }
+  var sueldoVirtual = valorHoraVirtual * horasVirtuales
+  var sueldoPresencial = valorHoraPresencial * horasPresenciales
+  var sueldo = sueldoVirtual + sueldoPresencial
 
-  var sueldo =
-    valorHora * horasVirtuales + (valorHora + 400) * horasPresenciales;
-
+  resultadoPresencialDiv.textContent =
+  "Sueldo: $" +
+  sueldoPresencial.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  resultadoVirualDiv.textContent =
+  "Sueldo: $" +
+  sueldoVirtual.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   resultadoDiv.textContent =
     "Sueldo: $" +
     sueldo.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-
   // Save data to localStorage
-  saveData(valorHora, horasVirtuales, horasPresenciales);
+  saveData(valorHoraPresencial, valorHoraVirtual, horasVirtuales, horasPresenciales);
   resetBtn.style.display = '';
 }
 
-function saveData(valorHora, horasVirtuales, horasPresenciales) {
+function saveData(valorHoraPresencial, valorHoraVirtual, horasVirtuales, horasPresenciales) {
     localStorage.setItem(
         'data',
         JSON.stringify({
-            valorHora: valorHora,
+            valorHoraPresencial: valorHoraPresencial,
+            valorHoraVirtual: valorHoraVirtual,
             horasVirtuales: horasVirtuales,
             horasPresenciales: horasPresenciales,
         })
@@ -58,9 +73,12 @@ function saveData(valorHora, horasVirtuales, horasPresenciales) {
 
 function resetCalculadora() {
     saveData(0,0,0)
-    valorHoraInput.value = null;
+    valorHoraVirtualInput.value = null;
+    valorHoraPresencialInput.value = null;
     horasVirtualesInput.value = null;
     horasPresencialesInput.value = null;
     resultadoDiv.textContent = "";
+    resultadoPresencialDiv.textContent = "";
+    resultadoVirualDiv.textContent = "";
     resetBtn.style.display = 'none';
 }
